ee9819b17f63f699c62c94b336b60b3fa5f5893b
Fiquell <fiquellh@gmail.com>
2024-06-17 16:05:59 +0800

    create a group

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'tailwindcss'
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {},
   },
   plugins: [],
