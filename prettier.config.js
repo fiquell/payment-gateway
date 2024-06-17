9fe2bd1e667400b736f167e882a9ac6f40e47fdc
Fiquell <fiquellh@gmail.com>
2024-06-17 15:48:30 +0800

    added tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  bracketSameLine: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
+
+export default config;
