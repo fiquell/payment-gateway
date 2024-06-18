c13473d05a0ad7e9c71f432f580813d435664354
Fiquell <fiquellh@gmail.com>
2024-06-19 02:56:23 +0800

    added dummyjson interface

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,59 @@
 // for information about these interfaces
 declare global {
   namespace App {
+    interface DummyJSON {
+      products: Product[]
+      total: number
+      skip: number
+      limit: number
+    }
+
+    interface Product {
+      id: number
+      title: string
+      description: string
+      category: string
+      price: number
+      discountPercentage: number
+      rating: number
+      stock: number
+      tags: string[]
+      brand?: string
+      sku: string
+      weight: number
+      dimensions: Dimensions
+      warrantyInformation: string
+      shippingInformation: string
+      availabilityStatus: string
+      reviews: Review[]
+      returnPolicy: string
+      minimumOrderQuantity: number
+      meta: Meta
+      images: string[]
+      thumbnail: string
+    }
+
+    interface Dimensions {
+      width: number
+      height: number
+      depth: number
+    }
+
+    interface Meta {
+      createdAt: Date
+      updatedAt: Date
+      barcode: string
+      qrCode: string
+    }
+
+    interface Review {
+      rating: number
+      comment: string
+      date: Date
+      reviewerName: string
+      reviewerEmail: string
+    }
+
     // interface Error {}
     // interface Locals {}
     // interface PageData {}
