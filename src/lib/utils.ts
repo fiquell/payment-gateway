16c0225d5d89858155f4ffbc0a03e8e7fe671f57
Fiquell <fiquellh@gmail.com>
2024-06-19 02:45:29 +0800

    added necessary utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,21 @@
+export const formatIDR = (price: number) => {
+  const formatter = new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+
+  return formatter.format(price).split('\u00A0').join('')
+}
+
+export const applyDiscount = (price: number, discount: number) => {
+  return price * (1 - discount / 100)
+}
+
+export const convertUSDToIDR = (price: number, discount: number = 0) => {
+  const exchangeRate = 16361
+  const discountedUSD = applyDiscount(price, discount)
+
+  return formatIDR(discountedUSD * exchangeRate)
+}
