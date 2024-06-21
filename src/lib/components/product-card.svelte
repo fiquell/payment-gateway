397ca35719c828e44901b1b02ae49140ba7b1845
Fiquell <fiquellh@gmail.com>
2024-06-21 15:35:00 +0800

    make it simple

--- src/lib/components/product-card.svelte
+++ src/lib/components/product-card.svelte
@@ -1,51 +1,45 @@
 <script lang="ts">
   import { convertUSDToIDR } from '$lib/utils'
-  import { kebabCase, titleCase } from 'string-ts'
+  import { ShoppingCart } from 'lucide-svelte'
 
   export let product: App.Product
 </script>
 
-<a
-  href="/{product.category}/{kebabCase(product.title)}"
-  class="group transition duration-300 ease-in-out hover:-translate-y-1">
-  <div
-    class="card card-compact w-72 bg-base-100 shadow-lg group-hover:shadow-xl">
-    <figure>
-      <img src={product.thumbnail} alt={product.title} />
-    </figure>
-    <div class="card-body">
-      <p class="truncate">{product.title}</p>
-      <div class="mb-1 mt-2 flex items-center gap-2">
+<div class="card card-compact w-72">
+  <figure class="rounded-2xl bg-base-100">
+    <img src={product.thumbnail} alt={product.title} />
+  </figure>
+  <div class="card-body">
+    <p class="truncate">{product.title}</p>
+    <div class="mb-1 mt-2 flex items-center gap-2">
+      <div>
+        <p class="font-semibold text-primary">
+          {convertUSDToIDR(product.price, product.discountPercentage)}
+        </p>
+      </div>
+      {#if product.discountPercentage >= 1}
         <div>
-          <p class="font-semibold text-primary">
-            {convertUSDToIDR(product.price, product.discountPercentage)}
-          </p>
+          <p class="line-through">{convertUSDToIDR(product.price)}</p>
         </div>
-        {#if product.discountPercentage >= 1}
-          <div>
-            <p class="line-through">{convertUSDToIDR(product.price)}</p>
-          </div>
-          <div class="badge badge-neutral">
-            <p>{product.discountPercentage.toFixed(0)}%</p>
-          </div>
+        <div class="badge badge-neutral">
+          <p>{product.discountPercentage.toFixed(0)}%</p>
+        </div>
+      {/if}
+    </div>
+    <div class="flex items-center gap-1">
+      {#each Array.from({ length: 5 }) as _, i}
+        {#if i < Math.floor(product.rating)}
+          <div class="mask mask-star-2 h-4 w-4 bg-warning" />
+        {:else}
+          <div class="mask mask-star-2 h-4 w-4 bg-neutral" />
         {/if}
-      </div>
-      <div class="flex items-center gap-1">
-        {#each Array.from({ length: 5 }) as _, i}
-          {#if i < Math.floor(product.rating)}
-            <div class="mask mask-star-2 h-4 w-4 bg-warning" />
-          {:else}
-            <div class="mask mask-star-2 h-4 w-4 bg-neutral" />
-          {/if}
-        {/each}
-      </div>
-      <div class="card-actions mt-4 justify-end">
-        {#each product.tags as tag}
-          <div class="badge badge-neutral badge-sm">
-            <p>{titleCase(tag)}</p>
-          </div>
-        {/each}
-      </div>
+      {/each}
+    </div>
+    <div class="card-actions mt-4">
+      <button class="btn btn-primary">
+        <ShoppingCart size={16} />
+        Add To Cart
+      </button>
     </div>
   </div>
-</a>
+</div>
--- src/routes/(app)/[category]/[product]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { page } from '$app/stores'
-</script>
-
-<section>
-  <p>{$page.params.product}</p>
-</section>
